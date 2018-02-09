916a1bd32dabd09e65837dd117d7b76e4a7f1172
Tan Chun Mun <vcfr67@motorolasolutions.com>
2018-02-09 14:32:49 -0500

    added asynchronous

--- DataBindingTutorial/app/src/main/java/com/example/ptt2vip/databindingtutorial/MainActivity.java
+++ DataBindingTutorial/app/src/main/java/com/example/ptt2vip/databindingtutorial/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ptt2vip.databindingtutorial;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ptt2vip.databindingtutorial.databinding.ActivityMainBinding;
 
@@ -16,6 +18,22 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binder = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         binder.setUser(myUser);
+        binder.executePendingBindings();
+
+        Button OnSubmit = (Button) findViewById(R.id.btnSubmit);
+        OnSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Thread thread = new Thread("New Thread") {
+                    public void run(){
+                        String strLastName = myUser.getLastName();
+                        String newStatement = "Hi "+ strLastName;
+                        myUser.setLastName(newStatement,0,0,newStatement.length());
+                    }
+                };
+                thread.start();
+
+            }
+        });
         /*ActivityMainBinding binder = ActivityMainBinding.inflate(getLayoutInflater(), container, attachToContainer);
         View view = binding.getRoot();
         TemperatureData temperatureData = // your data is created here
