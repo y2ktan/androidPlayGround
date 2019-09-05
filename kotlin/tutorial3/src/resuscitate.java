7428be087b9a2a9664761ef5ddcb7429114c5e2d
Tan Chun Mun <ChunMun.Tan@motorolasolutions.com>
2019-09-05 09:11:56 +0800

    add the sample code of resuscitate

---
+++ kotlin/tutorial3/src/resuscitate.java
@@ -0,0 +1,15 @@
+/* helper method to allow others to start/update PttAppWidget */
+    public static void resuscitate(Context context) {
+        /* with reference to the following stackoverflow answer:
+           https://stackoverflow.com/questions/3455123/programmatically-update-widget-from-activity-service-receiver
+         */
+        Intent intent = new Intent(context, PttAppWidget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        // Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
+        // since it seems the onUpdate() is only fired on that:
+        int[] ids = AppWidgetManager.getInstance(context).getAppWidgetIds((new ComponentName(context, PttAppWidget.class)));
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+        context.sendBroadcast(intent);
+    }
