644295643864bb09ceb64e8294398b16c0170305
y2ktan <chunmuntan@yahoo.com>
2020-05-13 14:55:49 +0800

    update keyguard code

--- wake_up_demo/app/src/main/java/com/ressphere/Alarm.kt
+++ wake_up_demo/app/src/main/java/com/ressphere/Alarm.kt
@@ -1,6 +1,7 @@
 package com.ressphere
 
 import android.annotation.SuppressLint
+import android.app.KeyguardManager
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,6 +13,7 @@ import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.app.ComponentActivity
 import androidx.core.app.ComponentActivity.ExtraData
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Build
 import android.view.Window
 import kotlinx.android.synthetic.main.activity_alarm.*
 
@@ -26,19 +28,33 @@ class Alarm : AppCompatActivity() {
 //        wl.acquire()
 
         this.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN or
-                    WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
-                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
-                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON,
-            (WindowManager.LayoutParams.FLAG_FULLSCREEN or
-                    WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
-                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
-                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
-        )
+        showScreenWhenLocked()
         setContentView(R.layout.activity_alarm)
         close_button.setOnClickListener {
             finish()
         }
     }
+
+
+    private fun showScreenWhenLocked() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager.requestDismissKeyguard(this, null)
+        } else {
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN or
+                            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON,
+                    (WindowManager.LayoutParams.FLAG_FULLSCREEN or
+                            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
+            )
+        }
+    }
+    
+
 }
